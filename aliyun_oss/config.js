b3eb3cae1c34a025c499b47e380568c6fdcf5367
JONE <31957754+jonexyz@users.noreply.github.com>
2018-09-20 14:30:50 +0800

    Update config.js

--- aliyun_oss/config.js
+++ aliyun_oss/config.js
@@ -4,7 +4,7 @@ var config = {
   AccessKeySecret: '',  // 阿里云 AccessKey ID
   OSSAccessKeyId: '',  // 阿里云 Access Key Secret
   timeout: 87600 , //这个是上传文件时Policy的失效时间
-   callbackUrl:'https://api.domain.com/api/index/index',  // 回调接口地址DEMO  具体参数自行配置
+  callbackUrl:'https://api.domain.com/api/index/index',  // 回调接口地址DEMO  具体参数自行配置
   callbackBody: 'bucket=${bucket}&object=${object}&etag=${etag}&size=${size}&mimeType=${mimeType}&my_var=${x:my_var}' // 回调返回的信息内容
 };
 module.exports = config
